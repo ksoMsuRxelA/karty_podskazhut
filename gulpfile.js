315902bab7c01415cd3354e0435ebf1829982378
Moskvitin R.A <moskvitin@ibrae.ac.ru>
2024-10-15 18:37:54 +0300

    feat: создал макет под оба размера в светлой теме

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,13 @@ function serve() {
   });
 }
 
+function fonts() {
+  return gulp
+    .src('./src/fonts/*.{woff,woff2}', { encoding: false })
+    .pipe(gulp.dest('./dist/fonts'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
 function html() {
   return gulp
     .src('./src/**/*.html')
@@ -31,7 +38,9 @@ function css() {
 
 function images() {
   return gulp
-    .src('./src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+    .src('./src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}', {
+      encoding: false,
+    })
     .pipe(gulp.dest('./dist/images'))
     .pipe(browserSync.reload({ stream: true }));
 }
@@ -40,12 +49,13 @@ function clean() {
   return del('./dist');
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, images));
+const build = gulp.series(clean, gulp.parallel(html, css, images, fonts));
 
 function watchFiles() {
   gulp.watch(['./src/**/*.html'], html);
   gulp.watch(['./src/**/*.css'], css);
   gulp.watch(['./src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['./src/fonts/*.{woff,woff2}'], fonts);
 }
 
 const watchapp = gulp.parallel(build, watchFiles, serve);
@@ -54,6 +64,7 @@ exports.clean = clean;
 exports.images = images;
 exports.css = css;
 exports.html = html;
+exports.fonts = fonts;
 
 exports.build = build;
 exports.watchapp = watchapp;
